23942863b3f2c010b91eee6a45492c077967ae93
consmi16 <consmi16@student.loreto.ac.uk>
2017-12-01 13:46:18 +0000

    Added Skeleton Login Page and new initiation screen

---
+++ Test1/Start.vb
@@ -0,0 +1,9 @@
+﻿Public Class Start
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Register.Show()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        frm_login.Show()
+    End Sub
+End Class

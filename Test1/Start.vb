401e37cab1cca0bd6f9203403e59fbcebe404ebd
consmi16 <consmi16@student.loreto.ac.uk>
2018-01-25 14:51:45 +0000

    me.visible

--- Test1/Start.vb
+++ Test1/Start.vb
@@ -1,15 +1,11 @@
 ﻿Public Class Start
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Register.Show()
-        Hide()
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click 'Once button is pressed'
+        Register.Show() 'Display to the user the egister screen so that they can create an account'
+        Me.Visible = False 'Make the screen not visible to my stakeholder'
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        frm_login.Show()
-        Hide()
-    End Sub
-
-    Private Sub Start_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click 'Once button is pressed'
+        frm_login.Show() 'Display to the user the login screen (This presumes they already have an account'
+        Me.Visible = False 'Make the screen not visible to my stakeholder'
     End Sub
 End Class
